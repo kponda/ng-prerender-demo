6fbd0c690507bc2d6c1bce2ff4cce45d7b009d29
Katsumi Honda <k@qox.jp>
2020-10-17 15:48:07 +0900

    スタイル変更

--- projects/manage/src/environments/environment.prod.ts
+++ projects/manage/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD6ZilmoozosPfijE_f90TwwOoRx0UDgHM",
+    authDomain: "ng-prerender.firebaseapp.com",
+    databaseURL: "https://ng-prerender.firebaseio.com",
+    projectId: "ng-prerender",
+    storageBucket: "ng-prerender.appspot.com",
+    messagingSenderId: "26885022002",
+    appId: "1:26885022002:web:2602942de52b528c42b72b",
+    measurementId: "G-JTD770GHK7"
+  }
 };
